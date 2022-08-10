f281be85498b7d28754d52aa88ac3a0abedcca9b
Rusland420 <rzfromyrizk@gmai.com>
2022-08-10 23:33:18 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBrckts=[]
+  let arrStr= [...str];
+  let rslt= arrStr.length/2
+if(arrStr.length%2!=0){
+  return false
 }
+for(let i=0;i<arrStr.length;i++){
+  for(let j=0;j<bracketsConfig.length;j++){
+    if(arrStr[i] === bracketsConfig[j][0] && arrStr[i+1] === bracketsConfig[j][1]){
+      arrStr.splice(i, 2);
+      i=-1
+    }
+    }
+  }
+ if(arrStr.length===0){
+  // console.log(arrBrckts)
+  return true
+ } 
+ else{
+  console.log(arrStr,str)
+  return false
+ }
+}  
+
+
+  
